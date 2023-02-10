5c62babf889f5a0934a56f59834d1a16332d3a87
Yuki <skool109@gmail.com>
2023-02-10 22:40:01 +0900

    feat: Create project
    프로젝트 파일 생성
    + .gitignore 추가
    + 패키지 구조 업로드를 위한 .gitkeep 파일 추가
    (추후 삭제 작업 필요)

---
+++ src/main/java/com/example/cardmonkey/CardMonkeyApplication.java
@@ -0,0 +1,12 @@
+package com.example.cardmonkey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardMonkeyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardMonkeyApplication.class, args);
+    }
+}

349f52ab25da6318eb705bfb871502d08c79adc4
Yuki <skool109@gmail.com>
2023-02-13 14:24:56 +0900

    feat: Update Entity
    리뷰 엔티티 구조 변경
    + Review 엔티티에서 review 1~10 필드 값 삭제
    + Review 와 Comment가 다대다 관계이므로 매핑테이블인 ReviewComment 생성
    
    Fixes: #6

--- src/main/java/com/example/cardmonkey/entity/ReviewComment.java
+++ src/main/java/com/example/cardmonkey/entity/ReviewComment.java
@@ -3,28 +3,23 @@ package com.example.cardmonkey.entity;
 import lombok.*;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class Review {
+public class ReviewComment {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "review_id")
+    @Column(name = "review_comment_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
+    @JoinColumn(name = "review_id")
+    private Review review;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "card_id")
-    private Card card;
-
-    @OneToMany(mappedBy = "review")
-    private List<ReviewComment> reviewComments = new ArrayList<>();
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
 }
